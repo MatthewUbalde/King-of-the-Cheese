8ecd11997596ef8a8194a417625ab8a83bd81dd5
FishHeadphones <matthew.ubalde@gmail.com>
2023-04-18 13:11:14 -0400

    Refractor the Camera Zoom script

--- scenes/camera/player_camera_2d.gd
+++ scenes/camera/player_camera_2d.gd
@@ -1,44 +1,48 @@
 extends Camera2D
 
-const ZOOM_INCREMENT = 0.25
-const MOUSE_SCROLL_DAMPING = 0.05
+signal zoom_update(zoom_value: Vector2, message: String)
+
+const ZOOM_MAX: Vector2 = Vector2(2.0, 2.0)
+const ZOOM_MIN: Vector2 = Vector2(0.25, 0.25)
+const ZOOM_INCREMENT: float = 0.25
+const MOUSE_SCROLL_DAMPING: float = 0.05
 
 @export var player: Player
  
 var tween: Tween
-var prev_global_position: Vector2 = global_position
 var zoom_value: Vector2 = Vector2.ONE
 var mouse_axis: float = 0.0
-
-func zoom_adjust(adjust: float) -> void:
-	zoom_value = clamp(zoom_value + Vector2(adjust, adjust), Vector2(.25, .25), Vector2(2.0, 2.0))
-	#zoom = zoom_value
-	smooth_set_zoom(zoom_value)
 
 
 func _input(event: InputEvent) -> void:
 	# Reset
 	if event.is_action_pressed("zoom_reset"):
-		smooth_set_zoom(Vector2.ONE)
-		print_debug("Camera Reset!")
+		zoom_value = Vector2.ONE
+		smooth_set_zoom(zoom_value)
+		zoom_update.emit(zoom_value, "Camera Reset")
 		return
 	
 	if event.is_action_pressed("zoom_in"):
 		zoom_adjust(ZOOM_INCREMENT)
-		print_debug("Camera Zoom in")
+		zoom_update.emit(zoom_value, "Camera Zoom in")
 		return
 	
 	if event.is_action_pressed("zoom_out"):
 		zoom_adjust(-ZOOM_INCREMENT)
-		print_debug("Camera Zoom out")
+		zoom_update.emit(zoom_value, "Camera Zoom out")
 		return
 	
 	mouse_axis = Input.get_axis("zoom_scroll_out", "zoom_scroll_in")
 	if mouse_axis != 0.0:
 		zoom_adjust(mouse_axis * MOUSE_SCROLL_DAMPING)
 		mouse_axis = 0.0 
-		print_debug("Camera Scroll")
+		zoom_update.emit(zoom_value, "Camera Scroll")
 		return
+
+
+func zoom_adjust(adjust: float) -> void:
+	zoom_value = clamp(zoom_value + Vector2(adjust, adjust), ZOOM_MIN, ZOOM_MAX) 
+	smooth_set_zoom(zoom_value)
 
 
 func smooth_set_zoom(value: Vector2) -> void:
@@ -46,5 +50,4 @@ func smooth_set_zoom(value: Vector2) -> void:
 		tween.kill()
 
 	tween = get_tree().create_tween()
-	tween.tween_property(self, "zoom", value, 0.4).set_trans(Tween.TRANS_CUBIC)
-	print_debug("Smoothing")
+	tween.tween_property(self, "zoom", value, 0.4).set_trans(Tween.TRANS_CUBIC) 
