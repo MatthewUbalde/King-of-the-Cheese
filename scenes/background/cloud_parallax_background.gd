478ea2a577fc7e46e2bf9cbd2cfebd4a5b688132
FishHeadphones <matthew.ubalde@gmail.com>
2023-04-18 15:44:51 -0400

    Adjusted the cloud parallax

--- scenes/background/cloud_parallax_background.gd
+++ scenes/background/cloud_parallax_background.gd
@@ -2,7 +2,6 @@ extends ParallaxBackground
 
 @export var clouds_speed := 25.0
 
-@onready var clouds_layer := %CloudsLayer
 
 func _process(delta):
 	scroll_base_offset -= Vector2(clouds_speed, 0) * delta
