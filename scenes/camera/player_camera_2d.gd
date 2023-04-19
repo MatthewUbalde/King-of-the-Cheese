97b9970b20df59a6427ec23924169a350c91a703
FishHeadphones <matthew.ubalde@gmail.com>
2023-04-19 19:15:10 -0400

    Added a precise zoom in and out option in player camera

--- scenes/camera/player_camera_2d.gd
+++ scenes/camera/player_camera_2d.gd
@@ -5,6 +5,7 @@ signal zoom_update(zoom_value: Vector2, message: String)
 const ZOOM_MAX: Vector2 = Vector2(2.0, 2.0)
 const ZOOM_MIN: Vector2 = Vector2(0.25, 0.25)
 const ZOOM_INCREMENT: float = 0.25
+const ZOOM_PRECISE_INCREMENT: float = 0.10
 const MOUSE_SCROLL_DAMPING: float = 0.05
 
 @export var player: Player
@@ -22,14 +23,22 @@ func _input(event: InputEvent) -> void:
 		zoom_update.emit(zoom_value, "Camera Reset")
 		return
 	
-	if event.is_action_pressed("zoom_in"):
+	if event.is_action_pressed("zoom_in_precise"):
+		zoom_adjust(ZOOM_PRECISE_INCREMENT)
+		zoom_update.emit(zoom_value, "Camera Zoom (0.05x) in")
+		return
+	elif event.is_action_pressed("zoom_in"):
 		zoom_adjust(ZOOM_INCREMENT)
-		zoom_update.emit(zoom_value, "Camera Zoom in")
+		zoom_update.emit(zoom_value, "Camera Zoom (0.25x) in")
 		return
 	
-	if event.is_action_pressed("zoom_out"):
+	if event.is_action_pressed("zoom_out_precise"):
+		zoom_adjust(-ZOOM_PRECISE_INCREMENT)
+		zoom_update.emit(zoom_value, "Camera Zoom (0.05x) out")
+		return
+	elif event.is_action_pressed("zoom_out"):
 		zoom_adjust(-ZOOM_INCREMENT)
-		zoom_update.emit(zoom_value, "Camera Zoom out")
+		zoom_update.emit(zoom_value, "Camera Zoom (0.25x) out")
 		return
 	
 	mouse_axis = Input.get_axis("zoom_scroll_out", "zoom_scroll_in")
