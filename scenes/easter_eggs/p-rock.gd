9c52897523dba1dbddf5ed4be6a2b10277e1a5ee
FishHeadphones <matthew.ubalde@gmail.com>
2023-04-24 21:48:33 -0400

    Fix a bug that doesn't detect if the player is nearby POOPATRON's rock

--- scenes/easter_eggs/p-rock.gd
+++ scenes/easter_eggs/p-rock.gd
@@ -1,9 +1,30 @@
 extends Node2D
 
+# I didn't recognized what the phrase meant, so I'm like really sorry.
+# I just bursted in laughter because of how loud it was, and accepted it.
+# so sorry sorry if this was weird. or i'm weird.
+
+var nearby_player: Player
+
+
 
 func _ready() -> void:
 	$Area2D.body_entered.connect(on_area_2d_body_entered)
+	$Area2D.body_exited.connect(on_area_2d_body_exited)
+
+
+func _input(event: InputEvent) -> void:
+	if nearby_player == null:
+		return
+	
+	if event.is_action_pressed("player_interact") && !$AudioStreamPlayer2D.playing:
+		$AudioStreamPlayer2D.play()
 
 
 func on_area_2d_body_entered(body: Node2D) -> void:
-	$AudioStreamPlayer2D.play()
+	if body is Player:
+		nearby_player = body
+
+
+func on_area_2d_body_exited(body: Node2D) -> void:
+	nearby_player = null
