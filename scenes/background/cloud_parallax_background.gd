d3d5fcb998da453b644465152863d7e5c3f5b361
FishHeadphones <matthew.ubalde@gmail.com>
2023-05-02 15:29:15 -0400

    Fix clouds going too fast after the splash screen
    There's an earlier commit that's the same, but forgot to commit this one

--- scenes/background/cloud_parallax_background.gd
+++ scenes/background/cloud_parallax_background.gd
@@ -1,9 +1,9 @@
 extends ParallaxBackground
 
-const SPEED_UP: float = 4.0 # Adds on to the speed_factor
+const SPEED_UP: float = 4.0 # Adds on to the speed_factor outside of this object
 
 @export var clouds_speed := 25.0
 @export var speed_factor := 1.0
 
 func _process(delta):
-	scroll_base_offset -= Vector2(clouds_speed * speed_factor * SPEED_UP, 0) * delta 
+	scroll_base_offset -= Vector2(clouds_speed * speed_factor, 0) * delta 
